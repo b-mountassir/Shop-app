fafc79c0ccb58d18c2e78e5a61b843ce71418b01
mountassir <mb.bouyebla@gmail.com>
2022-01-23 08:21:36 +0100

    add pagy

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,14 +1,15 @@
 class ProductsController < ApplicationController
     def index
         @category = Category.friendly.find(params[:category_id])
-        @products = @category.products
+        @pagy, @products = pagy(@category.products)
     end
     def show 
         @product = Product.friendly.find(params[:id])
     end
+    
     def search
         @q = Product.ransack(params[:q])
-        @products = @q.result(distinct: true)
+        @pagy, @products = pagy(@q.result(distinct: true))
     end
 
     
