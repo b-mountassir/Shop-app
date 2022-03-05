e3dcbf35d66c95d63f3bae9317ed78f0ea412cf9
mountassir <mb.bouyebla@gmail.com>
2022-03-05 16:00:08 +0100

    add omni auth

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -274,6 +274,9 @@ Devise.setup do |config|
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
 
+  config.omniauth :google_oauth2, ENV['GG_CLIENT'], ENV['GG_SECRET'], {}
+
+  OmniAuth.config.allowed_request_methods = %i[get]
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
   # change the failure app, you can configure them inside the config.warden block.
